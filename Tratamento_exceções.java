113715a475165f17dd5f7b4c2c18aa7677324975
Lucas <Lukinhasx248@gmail.com>
2025-05-13 14:00:00 -0300

    terminamos

--- Tratamento_exceções.java
+++ Tratamento_exceções.java
@@ -5,19 +5,25 @@ public class Tratamento_exceções {
     
     public static void main(String[] args) {
 
-        Integer numerador = 0;
-        Integer denominador = 0;
+        Integer numerador;
+        Integer denominador;
+        double divisao;
 
+        System.out.print("Digite o numerador: ");
         numerador = LER.nextInt();
+        System.out.print("Digite o denominador: ");
         denominador = LER.nextInt();
 
         try {
-            double divisao = numerador/denominador;
+            divisao = numerador/denominador;
             System.out.println("A divisão é: " + divisao);
         } catch (ArithmeticException execao) {
             System.out.println("Impossível dividir por 0");
         }finally{
-
+            System.out.println("Digite um denominador diferente de 0.");
+            denominador = LER.nextInt();
+            divisao = numerador/denominador;
+            System.out.println("Novo resultado: " + divisao);
         }
         
     }
--- Wrapper_Autoboxing.java
+++ Wrapper_Autoboxing.java
@@ -1,7 +1,30 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Wrapper_Autoboxing {
-    
+    public static Scanner LER = new Scanner(System.in);
 
     public static void main(String[] args) {
-        
+        ArrayList<Double> listaNotas = new ArrayList();
+        Double soma = 0d;
+
+        System.out.print("Digite a quantidade de notas que você quer adicionar: ");
+        Integer qtdNotas = LER.nextInt();
+
+        for (int i = 0; i < qtdNotas; i++) {
+            System.out.print("Digite a nota " + i +": ");
+            Double nota = LER.nextDouble();
+            listaNotas.add(nota);
+        }
+
+        for (int i = 0; i < listaNotas.size(); i++) {
+            soma += listaNotas.get(i);
+        }
+        System.out.println("A soma da nota dos alunos é: " + soma);
+
+        Double media = soma / qtdNotas;
+        System.out.print("A média do aluno é: " + media);
+
     }
 }
